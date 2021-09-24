7da97222e73b0d0262ea20cee7d1994acad98f39
aman_cjinc <aman.cjinc@gmail.com>
2021-09-24 21:11:19 +0530

    minor corrections

--- area.js
+++ area.js
@@ -2,6 +2,12 @@ var inp=document.querySelectorAll(".number");
 var btn=document.querySelector(".btn");
 var msg=document.querySelector(".msg");
 btn.addEventListener("click",()=>{
-  var a=0.5*inp[0].value*inp[1].value;
-  msg.innerText=`Length of hypotenuse is:  ${a}cm`;
+  if(Number(inp[0].value)>0&&Number(inp[1].value)>0)
+  { var a=0.5*inp[0].value*inp[1].value;
+    msg.innerText=`Area Of Triangle is:  ${a} square cm`;}
+    else{
+      MessageChannel.innerText="INVALID INPUT";
+    }
+  
+
 })
--- hypo.js
+++ hypo.js
@@ -2,6 +2,11 @@ var inp=document.querySelectorAll(".number");
 var btn=document.querySelector(".btn");
 var msg=document.querySelector(".msg");
 btn.addEventListener("click",()=>{
-  var a=Math.sqrt(Math.pow(Number(inp[0].value),2)+Math.pow(Number(inp[1].value),2));
-  msg.innerText=`Length of hypotenuse is:  ${a}cm`;
+  if(Number(inp[0].value)>0&&Number(inp[1].value)>0)
+  {var a=Math.sqrt(Math.pow(Number(inp[0].value),2)+Math.pow(Number(inp[1].value),2));
+    msg.innerText=`Length of hypotenuse is:  ${a}cm`;}
+    else{
+      MessageChannel.innerText="INVALID INPUT";
+    }
+  
 })
